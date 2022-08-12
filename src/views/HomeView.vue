9e8b4445183341951b9963509912e5ff600c251c
南浔沈万三 <cgmengyn376@163.com>
2022-08-12 22:07:33 +0800

    0812 测试提交2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,41 +12,41 @@
 </template>
 
 <script>
-// import Tabbar from "@/components/common/Tabbar.vue";
-// import Header from "@/components/home/Header.vue";
-// import Swiper from "@/components/home/Swiper.vue";
+import Tabbar from "@/components/common/Tabbar.vue";
+import Header from "@/components/home/Header.vue";
+import Swiper from "@/components/home/Swiper.vue";
 
-// export default {
-//   name: "HomeView",
-//   data() {
-//     return {
-//       selectedId: 0,
-//       items: [
-//         { label: "首页" },
-//         { label: "推荐" },
-//         { label: "铁观音" },
-//         { label: "乌龙茶" },
-//         { label: "红茶" },
-//         { label: "花茶" },
-//         { label: "普洱茶" },
-//         { label: "果茶" },
-//         { label: "茶具" },
-//       ],
-//       options: {
-//         activeColor: "#4f951c",
-//       },
-//     };
-//   },
-//   components: {
-//     Tabbar,
-//     Header,
-//     Swiper,
-//   },
-// };
+export default {
+  name: "HomeView",
+  data() {
+    return {
+      selectedId: 0,
+      items: [
+        { label: "首页" },
+        { label: "推荐" },
+        { label: "铁观音" },
+        { label: "乌龙茶" },
+        { label: "红茶" },
+        { label: "花茶" },
+        { label: "普洱茶" },
+        { label: "果茶" },
+        { label: "茶具" },
+      ],
+      options: {
+        activeColor: "#4f951c",
+      },
+    };
+  },
+  components: {
+    Tabbar,
+    Header,
+    Swiper,
+  },
+};
 </script>
 
 <style scoped>
-/* .ly-tab {
+.ly-tab {
   position: fixed;
   top: 1.6rem;
   left: 0;
@@ -57,5 +57,5 @@
 }
 ::v-deep .ly-tab-item {
   font-size: 16px;
-} */
+}
 </style>
